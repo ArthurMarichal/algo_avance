5de4e5b617e08d2f3a093d1aaa576fa91aed6989
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2021-11-22 13:17:16 +0100

    Recursive, Syracuse

--- recursive/src/com/company/Main.java
+++ recursive/src/com/company/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println("----------------------------------------");
         System.out.println("Suite de Fibonacci de 8 : " + fibonacci(8));
         System.out.println("Suite de Fibonacci de 9 : " + fibonacci(9));
+        System.out.println("----------------------------------------");
+        System.out.println("Conjecture de Syracuse de 19 : " + syracuse(15, 10));
     }
 
     public static int factoriel(int nombre) {
@@ -20,9 +22,20 @@ public class Main {
 
     public static int fibonacci(int nombre) {
         if (nombre > 1) {
-            return (fibonacci(nombre - 1)+fibonacci(nombre - 2));
+            return (fibonacci(nombre - 1) + fibonacci(nombre - 2));
         } else {
             return nombre;
         }
     }
+
+    public static int syracuse(int N, int nombre) {
+        if (nombre == 0)
+            return N;
+        int syr = syracuse(N,nombre-1);
+        if ( syr % 2 == 0)
+            return syr /2;
+        return syr *3 +1;
+    }
+
+
 }
