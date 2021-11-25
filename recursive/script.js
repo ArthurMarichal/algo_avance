a258702247ba70cfe16c640305b36bf3e81cf27e
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2021-11-25 14:01:23 +0100

    Labyrinthe, résolution (retrouver son chemin)

--- recursive/script.js
+++ recursive/script.js
@@ -1,9 +1,10 @@
-var size = 3;
+var size = 25;
 const currentMap = data[size]["ex-0"];
 var entrance;
 var exit;
 let s = []
 var i = 0;
+var path = [];
 
 
 function createMaze() {
@@ -11,12 +12,13 @@ function createMaze() {
     for (const currentCell of currentMap) {
         var cell = document.createElement("div");
         cell.setAttribute("id", i);
-        cell.style.width = "200px";
-        cell.style.height = "200px";
+        cell.style.width = "35px";
+        cell.style.height = "35px";
         cell.style.position = "absolute";
-        cell.style.left = currentCell.posY * 200 + 'px';
-        cell.style.top = currentCell.posX * 200 + 'px';
+        cell.style.left = currentCell.posY * 35 + 'px';
+        cell.style.top = currentCell.posX * 35 + 'px';
         cell.innerText = i;
+
 
 
         if (currentCell.walls[0]) {
@@ -32,65 +34,82 @@ function createMaze() {
             cell.style.borderLeft = "solid";
         }
         if (currentCell.entrance) {
-            cell.style.backgroundColor = "rgba(2,255,2,0.55)";
             entrance = i;
-            console.log(entrance);
+            cell.classList.add("entrance")
         } else if (currentCell.exit) {
-            cell.style.backgroundColor = "rgba(252,14,14,0.8)";
             exit = i;
-            console.log(exit);
+            cell.classList.add("exit")
+            console.log("la sortie : " + exit)
         } else {
-            cell.style.backgroundColor = "rgba(255,187,1,0.58)";
+            cell.classList.add("not-visited")
         }
         cell.style.borderColor = "#000000";
         document.body.appendChild(cell);
         i++
     }
-    dfs_iterative(currentMap, entrance);
+    return currentMap, entrance, exit;
 }
 
 function dfs_iterative(G, e) {
     s.push(e);
-
+    console.log(e)
 
     while (s.length !== 0) {
         let v = s.pop();
-        console.log(v);
+        console.log(G[v])
         if (!G[v].visited) {
             G[v].visited = true;
+            var cell = document.getElementById(v.toString());
+            cell.classList.add("visited")
             t = v - size;
             d = v + 1;
             b = v + size;
             g = v - 1;
-            if (!G[v].walls[0] && !G[t].visited) {
-                var cell = document.getElementById(v.toString());
-                cell.style.backgroundColor = "rgba(89,89,89,0.58)";
-                s.push(t)
+            if (v === exit) {
 
-            }
-            if (!G[v].walls[1] && !G[d].visited) {
-                var cell = document.getElementById(v.toString());
-                cell.style.backgroundColor = "rgba(89,89,89,0.58)";
-                s.push(d)
-            }
-            if (!G[v].walls[2] && !G[b].visited) {
-                var cell = document.getElementById(v.toString());
-                cell.style.backgroundColor = "rgba(89,89,89,0.58)";
-                s.push(b)
-            }
-            if (!G[v].walls[3] && !G[g].visited) {
-                var cell = document.getElementById(v.toString());
-                cell.style.backgroundColor = "rgba(89,89,89,0.58)";
-                s.push(g)
-            }
-            if (G[v] === exit){
                 console.log("DONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                break;
+                let w = v;
+                while (G[w].parent) {
+                    let p = G[w].parent;
+                    path.push(p)
+                    w = p;
+                    console.log(p)
+                }
+                console.log("DONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                console.log(path)
+            } else {
+                if (!G[v].walls[0] && !G[t].visited) {
+                    G[t].parent = v
+                    s.push(t)
+                }
+                if (!G[v].walls[1] && !G[d].visited) {
+                    G[d].parent = v
+                    s.push(d)
+                }
+                if (!G[v].walls[2] && !G[b].visited) {
+                    G[b].parent = v
+                    s.push(b)
+                }
+                if (!G[v].walls[3] && !G[g].visited) {
+                    G[g].parent = v
+                    s.push(g)
+                }
             }
         }
+    }
+    return path;
+}
+
+function parcours(path) {
+    while (path.length !== 0) {
+        console.log(path)
+        let parentCell = document.getElementById(path.pop())
+        parentCell.classList.add("path")
     }
 }
 
 createMaze();
+dfs_iterative(currentMap, entrance)
+parcours(path);
 
 
