ef3160f6194e7fc9ed0a60b9667d608b845538b6
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2021-11-25 16:28:24 +0100

    Labyrinthe, résolution (retrouver son chemin)

--- recursive/script.js
+++ recursive/script.js
@@ -1,4 +1,4 @@
-var size = 25;
+var size = 4;
 const currentMap = data[size]["ex-0"];
 var entrance;
 var exit;
@@ -108,8 +108,46 @@ function parcours(path) {
     }
 }
 
+
+function dfs_recursive(G, v){
+    if (!G[v].visited){
+        if (v === exit){
+            console.log("Done !")
+            return v;
+        }
+        G[v].visited = true;
+        var cell = document.getElementById(v.toString());
+        cell.classList.add("visited")
+        t = v - size;
+        d = v + 1;
+        b = v + size;
+        g = v - 1;
+        for (w in G[v]){
+            console.log(w)
+            if (!G[w].walls[0] && !G[t].visited) {
+                G[t].parent = v
+                s.push(t)
+            }
+            if (!G[w].walls[1] && !G[d].visited) {
+                G[d].parent = v
+                s.push(d)
+            }
+            if (!G[w].walls[2] && !G[b].visited) {
+                G[b].parent = v
+                s.push(b)
+            }
+            if (!G[v].walls[3] && !G[g].visited) {
+                G[w].parent = v
+                s.push(g)
+            }
+        }
+    }
+}
+
+
 createMaze();
 dfs_iterative(currentMap, entrance)
+//dfs_recursive(currentMap, entrance)
 parcours(path);
 
 
