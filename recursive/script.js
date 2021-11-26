1ca9add03d0ee3dbd87f0f3af4a01c0589656e32
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2021-11-26 14:07:40 +0100

    Labyrinthes, recursif + BFS

--- recursive/script.js
+++ recursive/script.js
@@ -1,5 +1,5 @@
-var size = 4;
-const currentMap = data[size]["ex-0"];
+var size = 25;
+const currentMap = data[size]["ex-2"];
 var entrance;
 var exit;
 let s = []
@@ -18,7 +18,6 @@ function createMaze() {
         cell.style.left = currentCell.posY * 35 + 'px';
         cell.style.top = currentCell.posX * 35 + 'px';
         cell.innerText = i;
-
 
 
         if (currentCell.walls[0]) {
@@ -101,53 +100,126 @@ function dfs_iterative(G, e) {
 }
 
 function parcours(path) {
+    // C'est pour DFS_itérative
     while (path.length !== 0) {
-        console.log(path)
         let parentCell = document.getElementById(path.pop())
         parentCell.classList.add("path")
     }
 }
+let counter = 0;
 
-
-function dfs_recursive(G, v){
-    if (!G[v].visited){
-        if (v === exit){
+function dfs_recursive(G, v) {
+    var cell = document.getElementById(v.toString());
+    if (!G[v].visited) {
+        if (v === exit) {
+            cell.classList.add("exit")
             console.log("Done !")
-            return v;
+            return [v];
         }
         G[v].visited = true;
-        var cell = document.getElementById(v.toString());
         cell.classList.add("visited")
-        t = v - size;
-        d = v + 1;
-        b = v + size;
-        g = v - 1;
-        for (w in G[v]){
-            console.log(w)
-            if (!G[w].walls[0] && !G[t].visited) {
-                G[t].parent = v
-                s.push(t)
-            }
-            if (!G[w].walls[1] && !G[d].visited) {
-                G[d].parent = v
-                s.push(d)
-            }
-            if (!G[w].walls[2] && !G[b].visited) {
-                G[b].parent = v
-                s.push(b)
-            }
-            if (!G[v].walls[3] && !G[g].visited) {
-                G[w].parent = v
-                s.push(g)
+        cell.classList.remove("not-visited")
+
+        const t = v - size;
+        const d = v + 1;
+        const b = v + size;
+        const g = v - 1;
+
+        if (!G[v].walls[0] && !G[t].visited) {
+            G[t].parent = v;
+            let p = dfs_recursive(G, t)
+            if (p){
+                p.push(v);
+                return p;
             }
         }
+
+        if (!G[v].walls[1] && !G[d].visited) {
+            G[d].parent = v;
+            let p = dfs_recursive(G, d)
+            if (p){
+                p.push(v);
+                return p;
+            }
+        }
+
+        if (!G[v].walls[2] && !G[b].visited) {
+            G[b].parent = v;
+            let p = dfs_recursive(G, b)
+            if (p){
+                p.push(v);
+                return p;
+            }
+        }
+
+        if (!G[v].walls[3] && !G[g].visited) {
+            G[g].parent = v;
+            let p = dfs_recursive(G, g)
+            if (p){
+                p.push(v);
+                return p;
+            }
+        }
+
+        if (G[v] === entrance)
+            cell.classList.add("entrance");
     }
 }
 
 
 createMaze();
-dfs_iterative(currentMap, entrance)
-//dfs_recursive(currentMap, entrance)
+//dfs_iterative(currentMap, entrance)
+path = dfs_recursive(currentMap, entrance)
 parcours(path);
+//bfs(currentMap, entrance);
 
+function bfs(G, e) {
+    let stack = [];
+    stack.push(e);
+    console.log(e)
 
+    while (stack.length !== 0) {
+        let v = stack.shift();
+        console.log(G[v]);
+        if (!G[v].visited) {
+            G[v].visited = true;
+            var cell = document.getElementById(v.toString());
+            cell.classList.add("visited")
+            t = v - size;
+            d = v + 1;
+            b = v + size;
+            g = v - 1;
+            if (v === exit) {
+
+                console.log("DONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                let w = v;
+                while (G[w].parent) {
+                    let p = G[w].parent;
+                    path.push(p)
+                    w = p;
+                    console.log(p)
+                }
+                console.log("DONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                console.log(path)
+            } else {
+                if (!G[v].walls[0] && !G[t].visited) {
+                    G[t].parent = v
+                    stack.push(t)
+                }
+                if (!G[v].walls[1] && !G[d].visited) {
+                    G[d].parent = v
+                    stack.push(d)
+                }
+                if (!G[v].walls[2] && !G[b].visited) {
+                    G[b].parent = v
+                    stack.push(b)
+                }
+                if (!G[v].walls[3] && !G[g].visited) {
+                    G[g].parent = v
+                    stack.push(g)
+                }
+            }
+        }
+    }
+    return path;
+}
